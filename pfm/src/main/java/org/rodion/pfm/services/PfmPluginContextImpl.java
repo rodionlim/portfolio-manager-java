6da5a79e837e33094a0b51aab1eb68d676566e29
Rodion <rodion.lim@hotmail.com>
2024-04-03 23:08:15 +0800

    feat: convert pfm to a subproject and add plugin-api project

---
+++ pfm/src/main/java/org/rodion/pfm/services/PfmPluginContextImpl.java
@@ -0,0 +1,3 @@
+package org.rodion.pfm.services;
+
+public class PfmPluginContextImpl {}
---
+++ plugin-api/src/main/java/org/rodion/pfm/Main.java
@@ -0,0 +1,7 @@
+package org.rodion.pfm;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

08d38f27f194b7b6848bc1e98ee8bdde15df33c2
Rodion <rodion.lim@hotmail.com>
2024-04-09 16:17:42 +0800

    feat: register rocks db factory with storage service

--- plugins/rocksdb/src/main/java/com/rodion/pfm/plugin/services/storage/rocksdb/RocksDBPlugin.java
+++ plugins/rocksdb/src/main/java/com/rodion/pfm/plugin/services/storage/rocksdb/RocksDBPlugin.java
@@ -9,9 +9,11 @@ import com.rodion.pfm.plugin.services.StorageService;
 import com.rodion.pfm.plugin.services.storage.SegmentIdentifier;
 import com.rodion.pfm.plugin.services.storage.rocksdb.configuration.RocksDBCLIOptions;
 import com.rodion.pfm.plugin.services.storage.rocksdb.configuration.RocksDBFactoryConfiguration;
+import com.rodion.pfm.plugin.services.storage.rocksdb.segmented.RocksDBKeyValueStorageFactory;
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import org.apache.commons.lang3.NotImplementedException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -21,9 +23,13 @@ public class RocksDBPlugin implements PfmPlugin {
 
   private static final String NAME = "rocksdb";
 
+  private final List<SegmentIdentifier> ignorableSegments = new ArrayList<>();
+
   private PfmContext context;
 
   private final RocksDBCLIOptions options;
+
+  private RocksDBKeyValueStorageFactory factory;
 
   public RocksDBPlugin() {
     options = RocksDBCLIOptions.create();
@@ -48,16 +54,35 @@ public class RocksDBPlugin implements PfmPlugin {
   }
 
   @Override
-  public void start() {}
+  public void start() {
+    logger.debug("Starting plugin.");
+    if (factory == null) {
+      logger.trace("Applied configuration: {}", options.toString());
+      createFactoriesAndRegisterWithStorageService();
+    }
+  }
 
   @Override
-  public void stop() {}
+  public void stop() {
+    logger.debug("Stopping plugin.");
+
+    try {
+      if (factory != null) {
+        factory.close();
+        factory = null;
+      }
+    } catch (final IOException e) {
+      logger.error("Failed to stop plugin: {}", e.getMessage(), e);
+    }
+  }
 
   private void createAndRegister(final StorageService service) {
     final List<SegmentIdentifier> segments = service.getAllSegmentIdentifiers();
     final Supplier<RocksDBFactoryConfiguration> configuration =
         Suppliers.memoize(options::toDomainObject);
-    throw new NotImplementedException();
+    factory = new RocksDBKeyValueStorageFactory(configuration, segments, ignorableSegments);
+    logger.info("Registering rocks DB kv storage factory with storage service");
+    service.registerKeyValueStorage(factory);
   }
 
   private void createFactoriesAndRegisterWithStorageService() {
